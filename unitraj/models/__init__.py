5e5f7bc331e10588354fe648da7e23139d7041dc
33zzstar <1299113812@qq.com>
2025-09-01 16:35:47 +0800

    9_1_1:Complete environment setup and standard initial version, reserved for future MoE expert encapsulation

---
+++ unitraj/models/__init__.py
@@ -0,0 +1,22 @@
+import os,sys 
+parentdir = '/home/zzs/zzs/unitraj__MOE/'
+sys.path.insert(0,parentdir) 
+from unitraj.models.autobot.autobot import AutoBotEgo
+from unitraj.models.mtr.MTR import MotionTransformer
+from unitraj.models.wayformer.wayformer import Wayformer
+from unitraj.models.smart.smart import SMART
+
+__all__ = {
+    'autobot': AutoBotEgo,
+    'wayformer': Wayformer,
+    'MTR': MotionTransformer,
+    'SMART': SMART,
+}
+
+
+def build_model(config):
+    model = __all__[config.method.model_name](
+        config=config
+    )
+
+    return model
